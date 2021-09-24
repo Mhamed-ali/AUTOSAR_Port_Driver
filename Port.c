e9d05a5763d21aa2ecf0e97e25d93c605368c6fd
Mhamed-ali <mohamedklay918@gmila.com>
2021-09-24 16:35:21 +0200

    first commit

---
+++ Port.c
@@ -0,0 +1,399 @@
+/******************************************************************************
+ *
+ * Module: Port
+ *
+ * File Name: Port.c
+ *
+ * Description: Source file for TM4C123GH6PM Microcontroller - Port Driver.
+ *
+ * Author: Mohamed Tarek
+ ******************************************************************************/
+
+#include "Port.h"
+#include "tm4c123gh6pm_registers.h"
+   
+#if (PORT_DEV_ERROR_DETECT == STD_ON)
+
+#include "Det.h"
+/* AUTOSAR Version checking between Det and Dio Modules */
+#if ((PORT_AR_MAJOR_VERSION != PORTAR_RELEASE_MAJOR_VERSION) || (PORT_AR_MINOR_VERSION != PORTAR_RELEASE_MINOR_VERSION) || (PORT_AR_PATCH_VERSION != PORTAR_RELEASE_PATCH_VERSION))
+#error "The AR version of port.h does not match the expected version"
+#endif
+
+#endif
+
+STATIC boolean Port_Status = PORT_NOT_INITIALIZED;
+
+/************************************************************************************
+* Service Name: Port_Init
+* Sync/Async: Synchronous
+* Reentrancy: Non Reentrant
+* Parameters (in): ConfigPtr - Pointer to configuration set.
+* Parameters (inout): None
+* Parameters (out): None
+* Return value: None
+* Description: Initializes the Port Driver module.
+************************************************************************************/
+void Port_Init(const Port_ConfigType *ConfigPtr)
+{
+
+#if (PORT_DEV_ERROR_DETECT == STD_ON)
+    /* check if the input configuration pointer is not a NULL_PTR */
+    if (NULL_PTR == ConfigPtr)
+    {
+        Det_ReportError(PORT_MODULE_ID, PORT_INSTANCE_ID, PORT_INIT_SID,
+                        PORT_E_PARAM_CONFIG);
+    }
+    else
+#endif
+        Port_Status = PORT_INITIALIZED;
+
+    for (uint8 i = 0; i < PORTS_PINS; i++)
+    {
+        Port_SetupGpioPin(&ConfigPtr->pins[i]);
+    }
+}
+
+/************************************************************************************
+* Service Name: Port_SetPinDirection
+* Sync/Async: Synchronous
+* Reentrancy: Reentrant
+* Parameters (in): Pin - Port Pin ID number.
+                   Direction - Port Pin Direction
+* Parameters (inout): None
+* Parameters (out): None
+* Return value: None
+* Description: Sets the port pin direction
+************************************************************************************/
+#if (PORT_SET_PIN_DIRECTION_API == STD_ON)
+void Port_SetPinDirection(Port_PinType Pin, Port_PinDirectionType Direction)
+{
+
+    if (Port_Status == PORT_NOT_INITIALIZED)
+    {
+
+#if (PORT_DEV_ERROR_DETECT == STD_ON)
+        Det_ReportError(PORT_MODULE_ID, PORT_INSTANCE_ID,
+                        PORT_SETPINDIRECTION_SID, PORT_E_UNINIT);
+#endif
+    }
+    else
+    {
+        if ((Pin < 0) || (Pin > PORTS_PINS))
+        {
+
+#if (PORT_DEV_ERROR_DETECT == STD_ON)
+            Det_ReportError(PORT_MODULE_ID, PORT_INSTANCE_ID,
+                            PORT_SETPINDIRECTION_SID, PORT_E_PARAM_PIN);
+#endif
+        }
+        else
+        {
+            if (Port_Configuration.pins[Pin].Pin_mode_changeable == PIN_CHANGABLE)
+            {
+                volatile uint32 portAddress = PortAddressFromPinId(Pin);
+                Pin = PinNumFromPinID(Pin);
+                if (Direction == PORT_PIN_OUT)
+                {
+                    SET_BIT(*(volatile uint32 *)((volatile uint8 *)portAddress + PORT_DIR_REG_OFFSET), Pin); /* Set the corresponding bit in the GPIODIR register to configure it as output pin */
+                }
+                else if (Direction == PORT_PIN_IN)
+                {
+                    CLEAR_BIT(*(volatile uint32 *)((volatile uint8 *)portAddress + PORT_DIR_REG_OFFSET), Pin); /* Clear the corresponding bit in the GPIODIR register to configure it as input pin */
+                }
+            }
+            else
+            {
+#if (PORT_DEV_ERROR_DETECT == STD_ON)
+                Det_ReportError(PORT_MODULE_ID, PORT_INSTANCE_ID,
+                                PORT_SETPINDIRECTION_SID, PORT_E_DIRECTION_UNCHANGEABLE);
+#endif
+            }
+        }
+    }
+}
+#else
+#endif
+
+/************************************************************************************
+* Service Name: Port_RefreshPortDirection
+* Sync/Async: Synchronous
+* Reentrancy: Non Reentrant
+* Parameters (in): None
+* Parameters (inout): None
+* Parameters (out): None
+* Return value: None
+* Description: Refreshes port direction.
+************************************************************************************/
+void Port_RefreshPortDirection(void)
+{
+    if (Port_Status == PORT_NOT_INITIALIZED)
+    {
+#if (PORT_DEV_ERROR_DETECT == STD_ON)
+        Det_ReportError(PORT_MODULE_ID, PORT_INSTANCE_ID,
+                        PORT_REFRESHPORTDIRECTION_SID, PORT_E_UNINIT);
+#endif
+    }
+    else
+    {
+        const volatile Port_PinConfigType *pin;
+        for (uint8 i = 0; i < PORTS_PINS; i++)
+        {
+            pin = &(Port_Configuration.pins[i]);
+
+            if (pin->Pin_direction_changeable != TRUE)
+            {
+                //const volatile Port_PinConfigType *pin = &Port_Config[Pin];
+                volatile uint32 portAddress = PortAddressFromPinId(i);
+                // volatile uint32 *portAddress = PortAddressFromPinId(pin);
+                if (pin->direction == PORT_PIN_OUT)
+                {
+                    SET_BIT(*(volatile uint32 *)((volatile uint8 *)portAddress + PORT_DIR_REG_OFFSET), pin->pin_num); /* Set the corresponding bit in the GPIODIR register to configure it as output pin */
+                }
+                else if (pin->direction == PORT_PIN_IN)
+                {
+                    CLEAR_BIT(*(volatile uint32 *)((volatile uint8 *)portAddress + PORT_DIR_REG_OFFSET), pin->pin_num); /* Clear the corresponding bit in the GPIODIR register to configure it as input pin */
+                }
+            }
+        }
+    }
+}
+
+/************************************************************************************
+* Service Name: Port_GetVersionInfo
+* Sync/Async: Synchronous
+* Reentrancy: Non Reentrant
+* Parameters (in): None
+* Parameters (inout): None
+* Parameters (out): versioninfo - Pointer to where to store the version information of this module.
+* Return value: None
+* Description: Returns the version information of this module.
+************************************************************************************/
+
+#if (PORT_VERSION_INFO_API == STD_ON)
+void PORT_GetVersionInfo(Std_VersionInfoType *versioninfo)
+{
+    if (Port_Status == PORT_NOT_INITIALIZED)
+    {
+#if (PORT_DEV_ERROR_DETECT == STD_ON)
+        Det_ReportError(PORT_MODULE_ID, PORT_INSTANCE_ID,
+                        PORT_GET_VERSION_INFO_SID, PORT_E_UNINIT);
+#endif
+    }
+    else
+    {
+#if (PORT_DEV_ERROR_DETECT == STD_ON)
+        /* Check if input pointer is not Null pointer */
+        if (NULL_PTR == versioninfo)
+        {
+            /* Report to DET  */
+            Det_ReportError(PORT_MODULE_ID, PORT_INSTANCE_ID,
+                            PORT_GET_VERSION_INFO_SID, PORT_E_PARAM_POINTER);
+        }
+        else
+#endif /* (PORT_DEV_ERROR_DETECT == STD_ON) */
+        {
+            /* Copy the vendor Id */
+            versioninfo->vendorID = (uint16)PORT_VENDOR_ID;
+            /* Copy the module Id */
+            versioninfo->moduleID = (uint16)PORT_MODULE_ID;
+            /* Copy Software Major Version */
+            versioninfo->sw_major_version = (uint8)PORT_SW_MAJOR_VERSION;
+            /* Copy Software Minor Version */
+            versioninfo->sw_minor_version = (uint8)PORT_SW_MINOR_VERSION;
+            /* Copy Software Patch Version */
+            versioninfo->sw_patch_version = (uint8)PORT_SW_PATCH_VERSION;
+        }
+    }
+}
+#endif
+
+/************************************************************************************
+* Service Name: Port_SetPinMode
+* Sync/Async: Synchronous
+* Reentrancy: Reentrant
+* Parameters (in): Pin - Port Pin ID number.
+*                  Mode - New Port Pin mode to be set on port pin.
+* Parameters (inout): None
+* Parameters (out): None
+* Return value: None
+* Description: Sets the port pin mode.
+************************************************************************************/
+#if (PORT_SET_PIN_MODE_API == STD_ON)
+
+void Port_SetPinMode(Port_PinType Pin, Port_PinModeType Mode)
+{ /* Check if the Driver is initialized before using this function */
+    if (Port_Status == PORT_NOT_INITIALIZED)
+    {
+#if (PORT_DEV_ERROR_DETECT == STD_ON)
+        Det_ReportError(PORT_MODULE_ID, PORT_INSTANCE_ID,
+                        PORT_SETPINMODE_SID, PORT_E_UNINIT);
+#endif
+    }
+    else
+    {
+
+        if ((Pin < 0) || (Pin > PORTS_PINS))
+        {
+#if (PORT_DEV_ERROR_DETECT == STD_ON)
+            Det_ReportError(PORT_MODULE_ID, PORT_INSTANCE_ID,
+                            PORT_SETPINMODE_SID, PORT_E_PARAM_PIN);
+#endif
+        }
+        else
+        {
+            if (Mode < 0 || Mode > 12)
+            {
+#if (PORT_DEV_ERROR_DETECT == STD_ON)
+                Det_ReportError(PORT_MODULE_ID, PORT_INSTANCE_ID,
+                                PORT_SETPINMODE_SID, PORT_E_PARAM_INVALID_MODE);
+#endif
+            }
+            else
+            {
+                const volatile Port_PinConfigType *pincfg = &(Port_Configuration.pins[Pin]);
+                // volatile uint32 *portAddress = PortAddressFromPinId(pin);
+                if (pincfg->Pin_mode_changeable == TRUE)
+                {
+                    volatile uint32 portAddress = PortAddressFromPinId(Pin);
+
+                    if (Mode == PORT_PIN_MODE_DIO_GPT || PORT_PIN_MODE_DIO_WDG)
+                    {
+                        CLEAR_BIT(*(volatile uint32 *)((volatile uint8 *)portAddress + PORT_ALT_FUNC_REG_OFFSET), pincfg->pin_num); /* Set the corresponding bit in Alternate function register to allow changes on this pin */
+                    }
+                    else
+                    {
+                        SET_BIT(*(volatile uint32 *)((volatile uint8 *)portAddress + PORT_ALT_FUNC_REG_OFFSET), pincfg->pin_num); /* Set the corresponding bit in Alternate function register to allow changes on this pin */
+                    }
+
+                    if (Mode == PORT_PIN_MODE_ADC)
+                    {
+                        SET_BIT(*(volatile uint32 *)((volatile uint8 *)portAddress + PORT_ANALOG_MODE_SEL_REG_OFFSET), pincfg->pin_num); /* Set the corresponding bit in Alternate function register to allow changes on this pin */
+                    }
+                    else
+                    {
+                        CLEAR_BIT(*(volatile uint32 *)((volatile uint8 *)portAddress + PORT_ANALOG_MODE_SEL_REG_OFFSET), pincfg->pin_num); /* Set the corresponding bit in Alternate function register to allow changes on this pin */
+                        SET_BIT(*(volatile uint32 *)((volatile uint8 *)portAddress + PORT_DIGITAL_ENABLE_REG_OFFSET), pincfg->pin_num);    /* Set the corresponding bit in Alternate function register to allow changes on this pin */
+                    }
+                    *(volatile uint32 *)((volatile uint8 *)portAddress + PORT_CTL_REG_OFFSET) &= ~(0x0000000F << (pincfg->pin_num * 4)); /* Set the corresponding bit in Alternate function register to allow changes on this pin */
+                    *(volatile uint32 *)((volatile uint8 *)portAddress + PORT_CTL_REG_OFFSET) |= Mode << (pincfg->pin_num * 4);          /* Set the corresponding bit in Alternate function register to allow changes on this pin */
+                }
+                else
+                {
+
+#if (PORT_DEV_ERROR_DETECT == STD_ON)
+                    Det_ReportError(PORT_MODULE_ID, PORT_INSTANCE_ID,
+                                    PORT_SETPINMODE_SID, PORT_E_MODE_UNCHANGEABLE);
+#endif
+                }
+            }
+        }
+    }
+}
+#endif
+
+
+
+/************************************************************************************
+* Service Name: Port_SetupGpioPin
+* Sync/Async: Synchronous
+* Reentrancy: reentrant
+* Parameters (in): ConfigPtr - Pointer to post-build configuration data
+* Parameters (inout): None
+* Parameters (out): None
+* Return value: None
+* Description: Function to Setup the pin configuration:
+*              - Setup the pin as Digital GPIO pin
+*              - Setup the direction of the GPIO pin
+*              - Provide initial value for o/p pin
+*              - Setup the internal resistor for i/p pin
+************************************************************************************/
+
+STATIC void Port_SetupGpioPin(const Port_PinConfigType *ConfigPtr)
+{
+    volatile uint32 *PortGpio_Ptr = NULL_PTR; /* point to the required Port Registers base address */
+    volatile uint32 delay = 0;
+
+    switch (ConfigPtr->port_num)
+    {
+    case 0:
+        PortGpio_Ptr = (volatile uint32 *)GPIO_PORTA_BASE_ADDRESS; /* PORTA Base Address */
+        break;
+    case 1:
+        PortGpio_Ptr = (volatile uint32 *)GPIO_PORTB_BASE_ADDRESS; /* PORTB Base Address */
+        break;
+    case 2:
+        PortGpio_Ptr = (volatile uint32 *)GPIO_PORTC_BASE_ADDRESS; /* PORTC Base Address */
+        break;
+    case 3:
+        PortGpio_Ptr = (volatile uint32 *)GPIO_PORTD_BASE_ADDRESS; /* PORTD Base Address */
+        break;
+    case 4:
+        PortGpio_Ptr = (volatile uint32 *)GPIO_PORTE_BASE_ADDRESS; /* PORTE Base Address */
+        break;
+    case 5:
+        PortGpio_Ptr = (volatile uint32 *)GPIO_PORTF_BASE_ADDRESS; /* PORTF Base Address */
+        break;
+    }
+
+    /* Enable clock for PORT and allow time for clock to start*/
+    SYSCTL_REGCGC2_REG |= (1 << ConfigPtr->port_num);
+    delay = SYSCTL_REGCGC2_REG;
+
+    if (((ConfigPtr->port_num == 3) && (ConfigPtr->pin_num == 7)) || ((ConfigPtr->port_num == 5) && (ConfigPtr->pin_num == 0))) /* PD7 or PF0 */
+    {
+        *(volatile uint32 *)((volatile uint8 *)PortGpio_Ptr + PORT_LOCK_REG_OFFSET) = 0x4C4F434B;                   /* Unlock the GPIOCR register */
+        SET_BIT(*(volatile uint32 *)((volatile uint8 *)PortGpio_Ptr + PORT_COMMIT_REG_OFFSET), ConfigPtr->pin_num); /* Set the corresponding bit in GPIOCR register to allow changes on this pin */
+    }
+    else if ((ConfigPtr->port_num == 2) && (ConfigPtr->pin_num <= 3)) /* PC0 to PC3 */
+    {
+        /* Do Nothing ...  this is the JTAG pins */
+        return;
+    }
+    else
+    {
+        /* Do Nothing ... No need to unlock the commit register for this pin */
+    }
+
+    CLEAR_BIT(*(volatile uint32 *)((volatile uint8 *)PortGpio_Ptr + PORT_ANALOG_MODE_SEL_REG_OFFSET), ConfigPtr->pin_num);   /* Clear the corresponding bit in the GPIOAMSEL register to disable analog functionality on this pin */
+    CLEAR_BIT(*(volatile uint32 *)((volatile uint8 *)PortGpio_Ptr + PORT_ALT_FUNC_REG_OFFSET), ConfigPtr->pin_num);          /* Disable Alternative function for this pin by clear the corresponding bit in GPIOAFSEL register */
+    *(volatile uint32 *)((volatile uint8 *)PortGpio_Ptr + PORT_CTL_REG_OFFSET) &= ~(0x0000000F << (ConfigPtr->pin_num * 4)); /* Clear the PMCx bits for this pin */
+
+    if (ConfigPtr->direction == PORT_PIN_OUT)
+    {
+        SET_BIT(*(volatile uint32 *)((volatile uint8 *)PortGpio_Ptr + PORT_DIR_REG_OFFSET), ConfigPtr->pin_num); /* Set the corresponding bit in the GPIODIR register to configure it as output pin */
+
+        if (ConfigPtr->initial_value == STD_HIGH)
+        {
+            SET_BIT(*(volatile uint32 *)((volatile uint8 *)PortGpio_Ptr + PORT_DATA_REG_OFFSET), ConfigPtr->pin_num); /* Set the corresponding bit in the GPIODATA register to provide initial value 1 */
+        }
+        else
+        {
+            CLEAR_BIT(*(volatile uint32 *)((volatile uint8 *)PortGpio_Ptr + PORT_DATA_REG_OFFSET), ConfigPtr->pin_num); /* Clear the corresponding bit in the GPIODATA register to provide initial value 0 */
+        }
+    }
+    else if (ConfigPtr->direction == PORT_PIN_IN)
+    {
+        CLEAR_BIT(*(volatile uint32 *)((volatile uint8 *)PortGpio_Ptr + PORT_DIR_REG_OFFSET), ConfigPtr->pin_num); /* Clear the corresponding bit in the GPIODIR register to configure it as input pin */
+
+        if (ConfigPtr->resistor == PULL_UP)
+        {
+            SET_BIT(*(volatile uint32 *)((volatile uint8 *)PortGpio_Ptr + PORT_PULL_UP_REG_OFFSET), ConfigPtr->pin_num); /* Set the corresponding bit in the GPIOPUR register to enable the internal pull up pin */
+        }
+        else if (ConfigPtr->resistor == PULL_DOWN)
+        {
+            SET_BIT(*(volatile uint32 *)((volatile uint8 *)PortGpio_Ptr + PORT_PULL_DOWN_REG_OFFSET), ConfigPtr->pin_num); /* Set the corresponding bit in the GPIOPDR register to enable the internal pull down pin */
+        }
+        else
+        {
+            CLEAR_BIT(*(volatile uint32 *)((volatile uint8 *)PortGpio_Ptr + PORT_PULL_UP_REG_OFFSET), ConfigPtr->pin_num);   /* Clear the corresponding bit in the GPIOPUR register to disable the internal pull up pin */
+            CLEAR_BIT(*(volatile uint32 *)((volatile uint8 *)PortGpio_Ptr + PORT_PULL_DOWN_REG_OFFSET), ConfigPtr->pin_num); /* Clear the corresponding bit in the GPIOPDR register to disable the internal pull down pin */
+        }
+    }
+    else
+    {
+        /* Do Nothing */
+    }
+
+    SET_BIT(*(volatile uint32 *)((volatile uint8 *)PortGpio_Ptr + PORT_DIGITAL_ENABLE_REG_OFFSET), ConfigPtr->pin_num); /* Set the corresponding bit in the GPIODEN register to enable digital functionality on this pin */
+}
---
+++ Port.h
@@ -0,0 +1,269 @@
+/******************************************************************************
+ *
+ * Module: Port
+ *
+ * File Name: Port.h
+ *
+ * Description: Header file for TM4C123GH6PM Microcontroller - Port Driver.
+ *
+ * Author: Mohamed Tarek
+ ******************************************************************************/
+
+#ifndef PORT_H
+#define PORT_H
+
+/* Id for the company in the AUTOSAR
+ * for example Mohamed Tarek's ID = 1000 :) */
+#define PORT_VENDOR_ID (1000U)
+
+/* PORT Module Id */
+#define PORT_MODULE_ID (120U)
+
+/* PORT Instance Id */
+#define PORT_INSTANCE_ID (0U)
+
+/*
+ * Module Version 1.0.0
+ */
+#define PORT_SW_MAJOR_VERSION (1U)
+#define PORT_SW_MINOR_VERSION (0U)
+#define PORT_SW_PATCH_VERSION (0U)
+
+/*
+ * AUTOSAR Version 4.0.3
+ */
+#define PORT_AR_RELEASE_MAJOR_VERSION (4U)
+#define PORT_AR_RELEASE_MINOR_VERSION (0U)
+#define PORT_AR_RELEASE_PATCH_VERSION (3U)
+
+/* Pre-compile option for Version Info API */
+#define PORT_GET_VERSION_INFO_SID (STD_ON)
+
+/* Pre-compile option for Set Pin Direction API*/
+#define PORT_SET_PIN_DIRECTION_API (STD_ON)
+
+/* Pre-compile option for Set Pin Direction API*/
+#define PORT_SET_PIN_MODE_API (STD_ON)
+/*
+ * Macros for PORT Status
+ */
+#define PORT_INITIALIZED (1U)
+#define PORT_NOT_INITIALIZED (0U)
+
+/*
+ * Macros for PIN CHANGABLE Status
+ */
+#define PIN_CHANGABLE (1U)
+#define PIN_UNCHANGABLE (0U)
+
+/* Standard AUTOSAR types */
+#include "Std_Types.h"
+
+/* AUTOSAR checking between Std Types and PORT Modules */
+#if ((STD_TYPES_AR_RELEASE_MAJOR_VERSION != PORT_AR_RELEASE_MAJOR_VERSION) || (STD_TYPES_AR_RELEASE_MINOR_VERSION != PORT_AR_RELEASE_MINOR_VERSION) || (STD_TYPES_AR_RELEASE_PATCH_VERSION != PORT_AR_RELEASE_PATCH_VERSION))
+#error "The AR version of Std_Types.h does not match the expected version"
+#endif
+
+/* PORT Pre-Compile Configuration Header file */
+#include "PORT_Cfg.h"
+
+/* AUTOSAR Version checking between PORT_Cfg.h and PORT.h files */
+#if ((PORT_CFG_AR_RELEASE_MAJOR_VERSION != PORT_AR_RELEASE_MAJOR_VERSION) || (PORT_CFG_AR_RELEASE_MINOR_VERSION != PORT_AR_RELEASE_MINOR_VERSION) || (PORT_CFG_AR_RELEASE_PATCH_VERSION != PORT_AR_RELEASE_PATCH_VERSION))
+#error "The AR version of PORT_Cfg.h does not match the expected version"
+#endif
+
+/* Software Version checking between PORT_Cfg.h and PORT.h files */
+#if ((PORT_CFG_SW_MAJOR_VERSION != PORT_SW_MAJOR_VERSION) || (PORT_CFG_SW_MINOR_VERSION != PORT_SW_MINOR_VERSION) || (PORT_CFG_SW_PATCH_VERSION != PORT_SW_PATCH_VERSION))
+#error "The SW version of PORT_Cfg.h does not match the expected version"
+#endif
+
+/* Non AUTOSAR files */
+#include "Common_Macros.h"
+#include "Port_Helper_Functions.h"
+#include "Det.h"
+
+/******************************************************************************
+ *                      API Service Id Macros                                 *
+ ******************************************************************************/
+
+/* Service ID for Port Init */
+#define PORT_INIT_SID (uint8)0x00
+
+/* Service ID for Port SetPinDirection */
+#define PORT_SETPINDIRECTION_SID (uint8)0x01
+
+/* Service ID for Port RefreshPortDirection */
+#define PORT_REFRESHPORTDIRECTION_SID (uint8)0x02
+
+/* Service ID for Port GetVersionInfo */
+#define PORT_GETVERSIONINFO_SID (uint8)0x03
+
+/* Service ID for Port SetPinMode */
+#define PORT_SETPINMODE_SID (uint8)0x04
+
+/*******************************************************************************
+ *                      DET Error Codes                                        *
+ *******************************************************************************/
+/* DET code to report Invalid Port Pin ID requested */
+#define PORT_E_PARAM_PIN (uint8)0x0A
+
+/* DET code to report Port Pin not configured as changeable */
+#define PORT_E_DIRECTION_UNCHANGEABLE (uint8)0x0B
+
+/* DET code to report API Port_Init service called with wrong parameter. */
+#define PORT_E_PARAM_CONFIG (uint8)0x0C
+
+/* DET code to report API Port_SetPinMode service called when mode is unchangeable. */
+#define PORT_E_PARAM_INVALID_MODE (uint8)0x0D
+
+/* DET code to report API Port_SetPinMode service called when mode is unchangeable. */
+#define PORT_E_MODE_UNCHANGEABLE (uint8)0x0E
+
+/* DET code to report API service called without module initializationp */
+#define PORT_E_UNINIT (uint8)0x0F
+
+/* DET code to report APIs called with a Null Pointer */
+#define PORT_E_PARAM_POINTER (uint8)0x10
+
+/*******************************************************************************
+ *                              Module Definitions                             *
+ *******************************************************************************/
+
+/* GPIO Registers base addresses */
+#define GPIO_PORTA_BASE_ADDRESS 0x40004000
+#define GPIO_PORTB_BASE_ADDRESS 0x40005000
+#define GPIO_PORTC_BASE_ADDRESS 0x40006000
+#define GPIO_PORTD_BASE_ADDRESS 0x40007000
+#define GPIO_PORTE_BASE_ADDRESS 0x40024000
+#define GPIO_PORTF_BASE_ADDRESS 0x40025000
+
+/* GPIO Registers offset addresses */
+#define PORT_DATA_REG_OFFSET 0x3FC
+#define PORT_DIR_REG_OFFSET 0x400
+#define PORT_ALT_FUNC_REG_OFFSET 0x420
+#define PORT_PULL_UP_REG_OFFSET 0x510
+#define PORT_PULL_DOWN_REG_OFFSET 0x514
+#define PORT_DIGITAL_ENABLE_REG_OFFSET 0x51C
+#define PORT_LOCK_REG_OFFSET 0x520
+#define PORT_COMMIT_REG_OFFSET 0x524
+#define PORT_ANALOG_MODE_SEL_REG_OFFSET 0x528
+#define PORT_CTL_REG_OFFSET 0x52C
+
+#define PORTS_PINS 43
+
+/*******************************************************************************
+ *                              Module Data Types                              *
+ *******************************************************************************/
+
+/* Description: Enum to hold PIN mode */
+
+typedef enum
+{
+    PORT_PIN_MODE_ADC,
+    PORT_PIN_MODE_CAN,
+    PORT_PIN_MODE_DIO,
+    PORT_PIN_MODE_DIO_GPT,
+    PORT_PIN_MODE_DIO_WDG,
+    PORT_PIN_MODE_GPT,
+    PORT_PIN_MODE_WDG,
+    PORT_PIN_MODE_FLEXRAY,
+    PORT_PIN_MODE_ICU,
+    PORT_PIN_MODE_LIN,
+    PORT_PIN_MODE_MEM,
+    PORT_PIN_MODE_PWM,
+    PORT_PIN_MODE_SPI
+
+} Port_Pin_mode;
+typedef uint16 Port_PinModeType;
+
+/* Description: Enum to hold PIN Direction */
+
+typedef enum
+{
+    PORT_PIN_IN,
+    PORT_PIN_OUT
+} Port_PinDirectionType;
+
+/* Description: Enum to hold PIN level */
+
+typedef enum
+{
+    PORT_PIN_LEVEL_LOW,
+    PORT_PIN_LEVEL_HIGH
+
+} Port_Pin_level_Init_Value;
+
+/* Description: Enum to hold internal resistor type for PIN */
+typedef enum
+{
+    OFF,
+    PULL_UP,
+    PULL_DOWN
+} Port_InternalResistor;
+
+/* Description: Structure to configure each individual PIN:
+ *	1. the pin mode of the pin eg(adc,spi,...)
+ *	2. the direction of pin --> INPUT or OUTPUT
+ *  3. the initial value of pin --> high or low
+ *  4. the changeability of the pin during runtime  
+ */
+typedef struct
+{
+    uint8 port_num;
+    uint8 pin_num;
+    Port_PinDirectionType direction;
+    Port_Pin_mode pin_mode;
+    Port_Pin_level_Init_Value initial_value;
+    boolean Pin_direction_changeable;
+    boolean Pin_mode_changeable;
+    Port_InternalResistor resistor;
+
+    // Port_PinDirection direction;
+    // uint8 initial_value;
+
+    // Port_PinModeType pin_mode;
+} Port_PinConfigType;
+
+typedef struct
+{
+    Port_PinConfigType pins[PORTS_PINS];
+} Port_ConfigType;
+
+//typedef uint8 Port_PinType;
+
+// global variable to the Port_Configuration
+extern const Port_ConfigType Port_Configuration;
+
+typedef uint8 Port_PinType;
+
+/*******************************************************************************
+ *                      Function Prototypes                                    *
+ *******************************************************************************/
+
+/************************************************************************************
+* Description: Function to Setup the pin configuration:
+*              - Setup the pin as Digital GPIO pin
+*              - Setup the direction of the GPIO pin
+*              - Setup the internal resistor for i/p pin
+************************************************************************************/
+
+// Description: Initializes the Port Driver module.
+void Port_Init(const Port_ConfigType *ConfigPtr);
+
+/* Description: Sets the port pin direction */
+void Port_SetPinDirection(Port_PinType Pin, Port_PinDirectionType Direction);
+
+// Description: Refreshes port direction.
+void Port_RefreshPortDirection(void);
+
+// Description: Returns the version information of this module.
+void Port_GetVersionInfo(Std_VersionInfoType *versioninfo);
+
+// Description: Sets the port pin mode.
+void Port_SetPinMode(Port_PinType Pin, Port_PinModeType Mode);
+
+
+STATIC void Port_SetupGpioPin(const Port_PinConfigType *ConfigPtr);
+
+
+#endif /* PORT_H */
